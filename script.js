5fcf1634188be588876a482fab763b4b3a8daa53
mari78f9 <mariavbrendstrup@hotmail.com>
2019-06-17 19:48:51 +0200

    js

---
+++ script.js
@@ -0,0 +1,20 @@
+var date = new Date();
+var day = date.getDate();
+var year = date.getFullYear();
+
+var month = date.getMonth();
+var monthLabels = ["Januar", "Februar", "Marts", "April", "Maj", "Juni", "Juli", "August", "September", "Oktober", "November", "December"];
+
+var weekDay = date.getDay();
+var weekDayLabels = ["Søndag", "Mandag", "Tirsdag", "Onsdag", "Torsdag", "Fredag", "Lørdag"];
+
+//create function to grab day, month, date, year
+function Day() {
+    month = monthLabels[month];
+    weekDay = weekDayLabels[weekDay];
+
+    document.getElementById("day").innerHTML = "I dag er" + "<span>" + weekDay.toUpperCase() + "</span>";
+    document.getElementById("date").innerHTML = month.toUpperCase() + " " + "<span>" + day + "</span>" + year;
+}
+
+Day();
