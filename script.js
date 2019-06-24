c95b639e13b75ff8ca237872c32d4c6da927d131
mari78f9 <mariavbrendstrup@hotmail.com>
2019-06-24 18:48:07 +0200

    NYT

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-var date = new Date();
-var day = date.getDate();
-var year = date.getFullYear();
+let date = new Date();
+let day = date.getDate();
+let year = date.getFullYear();
 
-var month = date.getMonth();
-var monthLabels = ["Januar", "Februar", "Marts", "April", "Maj", "Juni", "Juli", "August", "September", "Oktober", "November", "December"];
+let month = date.getMonth();
+let monthLabels = ["Januar", "Februar", "Marts", "April", "Maj", "Juni", "Juli", "August", "September", "Oktober", "November", "December"];
 
-var weekDay = date.getDay();
-var weekDayLabels = ["Søndag", "Mandag", "Tirsdag", "Onsdag", "Torsdag", "Fredag", "Lørdag"];
+let weekDay = date.getDay();
+let weekDayLabels = ["Søndag", "Mandag", "Tirsdag", "Onsdag", "Torsdag", "Fredag", "Lørdag"];
 
 //create function to grab day, month, date, year
 function Day() {
     month = monthLabels[month];
     weekDay = weekDayLabels[weekDay];
 
-    document.getElementById("day").innerHTML = "I DAG ER" + "<span>" + weekDay.toUpperCase() + "</span>";
-    document.getElementById("date").innerHTML = month.toUpperCase() + " " + "<span>" + day + "</span>" + year;
+    document.querySelector("#day").innerHTML = "I DAG ER" + "<span>" + weekDay.toUpperCase() + "</span>";
+    document.querySelector("#date").innerHTML = month.toUpperCase() + " " + "<span>" + day + "</span>" + year;
 }
 
 Day();
